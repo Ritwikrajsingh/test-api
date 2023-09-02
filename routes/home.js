17acee1c96826801140fcd119f2f2e33cba8d0dd
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-09-02 12:17:18 +0530

    removed unnecessary routes

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const homeRoute = express.Router();
-const homeController = require('../controllers/home')
-
-homeRoute.get('/', homeController.getBlogs)
-// homeRoute.post('/', homeController.getBlogs)
-// homeRoute.post('/', homeController.uploadBanner)
-
-module.exports = homeRoute;
