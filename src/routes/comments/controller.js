edd79ab8a71c4aa9042d742b14ff34c9a2cb9c53
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2023-09-11 18:01:17 +0530

    minor fixes

--- src/routes/comments/controller.js
+++ src/routes/comments/controller.js
@@ -4,6 +4,7 @@ const isValidUUIDv4 = (uuid) => {
     const uuidPattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
     return uuidPattern.test(uuid);
 }
+
 module.exports = {
     createComment: async (req, res) => {
         try {
--- src/routes/welcome.js
+++ src/routes/welcome.js
@@ -9,7 +9,7 @@ const welcomeController = require('../controllers/welcome')
 // welcome.post('/comment/:blogId', welcomeController.createComment)
 // welcome.get('/comment/:blogId', welcomeController.getComment)
 
-welcome.post('/reply/:commentId', welcomeController.createReply)
+// welcome.post('/reply/:commentId', welcomeController.createReply)
 welcome.get('/reply/:commentId', welcomeController.getReply)
 
 module.exports = welcome
